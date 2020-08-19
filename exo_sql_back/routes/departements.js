a7569b1cf90f301561631a2a67477d256c8034f6
Vincent-ka <vincent.ka2893@gmail.com>
2020-08-19 11:46:51 +0200

    ajout du back

---
+++ exo_sql_back/routes/departements.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'villes_fr'
+});
+
+
+connection.connect();
+
+connection.query('SELECT * FROM departement', function (error, results, fields) {
+    if (error) throw error;
+    router.get("/", async (req, res, next) => {
+        try {
+          res.json(results);
+        } catch (err) {
+          next(err);
+        }
+      });
+});
+
+connection.end();
+
+// GET
+
+
+module.exports = router;
